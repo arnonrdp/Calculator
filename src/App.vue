181d1c6c6822dd8bde7330a25b46dc334713143f
arnonrdp <arnonrdp@gmail.com>
2021-08-28 23:51:00 -0300

    Added main styles

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,13 @@ body {
 	margin: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	color: #fff;
+	background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
 }
 </style>
